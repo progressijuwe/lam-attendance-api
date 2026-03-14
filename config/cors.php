1593c85d5b36e9d5e258bfd8a1d483f6ab73a734
progressijuwe <yenumijuwe@gmail.com>
2026-03-14 19:34:13 +0100

    fix cors for production

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173',
-        'https://lam-attendance-react.vercel.app/',
+        'https://lam-attendance-react.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
